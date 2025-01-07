5d7d8adb8a5e0d9b759f26a8c9bc89e3bbdbf1fe
Beniamin Zaborski <beniamin.zaborski@gmail.com>
2025-01-07 18:14:17 +0100

    GitHub workflows

---
+++ src/Bz.F8t.ChatBot.WebAPI/Controllers/ChatController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bz.F8t.ChatBot.WebAPI.Controllers;
+
+public class ChatRequest
+{
+    public string Query { get; set; }
+}
+
+[Route("api/[controller]")]
+[ApiController]
+public class ChatController : ControllerBase
+{
+    [HttpPost]
+    public async Task<string> AskAsync([FromBody] ChatRequest request)
+    {
+        return "Chatbot answer!";
+    }
+}
